6dae2d3b801fe9f01f5ad2594881ff800badc72a
suhas <suhasuluvaru739@gmail.com>
2026-03-17 15:03:29 +0530

    first commit

--- server.js
+++ server.js
@@ -1,103 +1,58 @@
 const express = require("express")
-const mongoose = require("mongoose")
 const cors = require("cors")
+const mongoose = require("mongoose")
+const path = require("path")
 
 const app = express()
 
-app.use(express.json())
+// Middleware
 app.use(cors())
+app.use(express.json())
 
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/public")))
+
+// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/phonestore")
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err))
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err))
 
-
-// USER MODEL
+// User Schema
 const UserSchema = new mongoose.Schema({
-username:String,
-password:String
+  username: String,
+  password: String
 })
 
-const User = mongoose.model("User",UserSchema)
-
-
-// PHONE MODEL
-const PhoneSchema = new mongoose.Schema({
-name:String,
-price:Number,
-image:String
-})
-
-const Phone = mongoose.model("Phone",PhoneSchema)
-
-
+const User = mongoose.model("User", UserSchema)
 
 // REGISTER
-app.post("/register", async (req,res)=>{
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body
 
-const {username,password} = req.body
+    const newUser = new User({ username, password })
+    await newUser.save()
 
-const newUser = new User({username,password})
-
-await newUser.save()
-
-res.json({message:"User registered"})
-
+    res.json({ success: true })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
 })
-
-
 
 // LOGIN
-app.post("/login", async (req,res)=>{
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body
 
-const {username,password} = req.body
+  const user = await User.findOne({ username, password })
 
-const user = await User.findOne({username,password})
-
-if(user){
-res.json({success:true})
-}else{
-res.json({success:false})
-}
-
+  if (user) {
+    res.json({ success: true })
+  } else {
+    res.json({ success: false })
+  }
 })
 
-
-
-// GET PHONES
-app.get("/phones", async (req,res)=>{
-
-const phones = await Phone.find()
-
-res.json(phones)
-
-})
-
-
-
-// ADD PHONES (RUN ONCE)
-app.get("/addphones", async (req,res)=>{
-
-await Phone.insertMany([
-
-{name:"iPhone 15",price:999,image:"https://m.media-amazon.com/images/I/61bK6PMOC3L.jpg"},
-{name:"iPhone 14",price:899,image:"https://m.media-amazon.com/images/I/61cwywLZR-L.jpg"},
-{name:"Samsung Galaxy S23",price:899,image:"https://m.media-amazon.com/images/I/71goZuIha-L.jpg"},
-{name:"Samsung Galaxy S22",price:799,image:"https://m.media-amazon.com/images/I/71z2K0QfPBL.jpg"},
-{name:"OnePlus 12",price:799,image:"https://m.media-amazon.com/images/I/61BAuSC0UnL.jpg"},
-{name:"OnePlus 11",price:699,image:"https://m.media-amazon.com/images/I/61amb0CfMGL.jpg"},
-{name:"Google Pixel 8",price:699,image:"https://m.media-amazon.com/images/I/61bK6PMOC3L.jpg"},
-{name:"Xiaomi 14",price:650,image:"https://m.media-amazon.com/images/I/51XU5oG3LxL.jpg"},
-{name:"Realme GT 6",price:550,image:"https://m.media-amazon.com/images/I/61amb0CfMGL.jpg"},
-{name:"Vivo X100",price:720,image:"https://m.media-amazon.com/images/I/71w3oJ7aWyL.jpg"}
-
-])
-
-res.send("Phones Added")
-
-})
-
-
-app.listen(3000,()=>{
-console.log("Server running on http://localhost:3000")
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
 })
